301e669730143a9ee6b11b849a62afedbe092860
dengqi <906262038@qq.com>
2019-05-17 16:01:28 +0800

    添加抽样“

--- src/CPU/throughput.cpp
+++ src/CPU/throughput.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unordered_map>
 #include <vector>
+#include <cmath>
 
 #include "./elastic/ElasticSketch.h"
 #include "./BloomFilter/bloomfilter.h"
@@ -54,36 +55,41 @@ int main()
 
 	for(int datafileCnt = START_FILE_NO; datafileCnt <= END_FILE_NO; ++datafileCnt)
 	{
-		elastic = NULL;
-
-		timespec time1, time2;
-		long long resns;
-		int packet_cnt = (int)traces[datafileCnt - 1].size();
-
-		uint8_t **keys = new uint8_t*[(int)traces[datafileCnt - 1].size()];
-		for(int i = 0; i < (int)traces[datafileCnt - 1].size(); ++i)
+		for(int sample=10;sample<=100;sample+=10)
 		{
-			keys[i] = new uint8_t[13];
-			memcpy(keys[i], traces[datafileCnt - 1][i].key, 13);
+			elastic = NULL;
+
+			timespec time1, time2;
+			long long resns;
+			int packet_cnt = (int)traces[datafileCnt - 1].size();
+
+			uint8_t **keys = new uint8_t*[(int)traces[datafileCnt - 1].size()];
+			for(int i = 0; i < (int)traces[datafileCnt - 1].size(); ++i)
+			{
+				keys[i] = new uint8_t[13];
+				memcpy(keys[i], traces[datafileCnt - 1][i].key, 13);
+			}
+
+			clock_gettime(CLOCK_MONOTONIC, &time1);
+			for(int t = 0; t < test_cycles; ++t)
+			{
+				elastic = new ElasticSketch<BUCKET_NUM, TOT_MEM_IN_BYTES>();
+				for(int i = 0; i < packet_cnt; ++i)
+					if(i%(int)ceil(100.0/sample)==0)
+						elastic->insert(keys[i]);
+				delete elastic;
+			}
+			clock_gettime(CLOCK_MONOTONIC, &time2);
+			resns = (long long)(time2.tv_sec - time1.tv_sec) * 1000000000LL + (time2.tv_nsec - time1.tv_nsec);
+			double th = (double)1000.0 * test_cycles * packet_cnt / resns;
+
+			/* free memory */
+			for(int i = 0; i < (int)traces[datafileCnt - 1].size(); ++i)
+				delete[] keys[i];
+			delete[] keys;
+
+			printf("%d.dat sampling rate:%d throughput is %lf mbps\n",datafileCnt,sample,th);
 		}
-
-		clock_gettime(CLOCK_MONOTONIC, &time1);
-		for(int t = 0; t < test_cycles; ++t)
-		{
-			elastic = new ElasticSketch<BUCKET_NUM, TOT_MEM_IN_BYTES>();
-			for(int i = 0; i < packet_cnt; ++i)
-				elastic->insert(keys[i]);
-			delete elastic;
-		}
-		clock_gettime(CLOCK_MONOTONIC, &time2);
-		resns = (long long)(time2.tv_sec - time1.tv_sec) * 1000000000LL + (time2.tv_nsec - time1.tv_nsec);
-		double th = (double)1000.0 * test_cycles * packet_cnt / resns;
-
-		/* free memory */
-		for(int i = 0; i < (int)traces[datafileCnt - 1].size(); ++i)
-			delete[] keys[i];
-		delete[] keys;
-
-		printf("throughput is %lf mbps\n", th);
 	}
+
 }
