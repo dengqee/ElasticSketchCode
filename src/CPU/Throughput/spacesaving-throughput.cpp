46d9563525dab6d4d558d1b1cfe1a8ae1ae5eaa5
dengqi <906262038@qq.com>
2019-05-24 21:35:48 +0800

    spacesaving-throughput.cpp finished

--- src/CPU/Throughput/spacesaving-throughput.cpp
+++ src/CPU/Throughput/spacesaving-throughput.cpp
@@ -8,6 +8,9 @@
 #include <stdlib.h>
 #include <unordered_map>
 #include <vector>
+#include <cmath>
+#include <set>
+#include <iostream>
 
 
 #include "../SpaceSaving/SpaceSaving.h"
@@ -57,39 +60,79 @@ int main()
 
 
 	for(int datafileCnt = START_FILE_NO; datafileCnt <= END_FILE_NO; ++datafileCnt)
-	{
-		ss = NULL;
-
-		timespec time1, time2;
-		long long resns;
-		int packet_cnt = (int)traces[datafileCnt - 1].size();
-
-		uint8_t **keys = new uint8_t*[(int)traces[datafileCnt - 1].size()];
-		for(int i = 0; i < (int)traces[datafileCnt - 1].size(); ++i)
 		{
-			keys[i] = new uint8_t[13];
-			memcpy(keys[i], traces[datafileCnt - 1][i].key, 13);
+			set<string>flowID;
+
+
+			ss = NULL;
+
+			timespec time1, time2;
+			long long resns;
+			int packet_cnt = (int)traces[datafileCnt - 1].size();
+
+			uint8_t **keys = new uint8_t*[(int)traces[datafileCnt - 1].size()];
+			for(int i = 0; i < (int)traces[datafileCnt - 1].size(); ++i)
+			{
+				keys[i] = new uint8_t[13];
+				memcpy(keys[i], traces[datafileCnt - 1][i].key, 13);
+				string key_str((const char*)(traces[datafileCnt - 1][i].key), 4);
+				flowID.insert(key_str);
+			}
+			int flow_cnt=flowID.size();
+			set<string>::iterator flow_it=flowID.begin();
+			set<string>flow_ins;
+			int flow_ins_num=0;
+			for(int sample=1;sample<=10;sample++)
+			{
+				flow_ins_num+=flow_cnt/10;
+				for(;flow_ins.size()<=flow_ins_num;flow_it++)
+				{
+					if(flow_it==flowID.end())
+						break;
+					flow_ins.insert(*flow_it);
+				}
+				bool tag[3000000];
+				for(int i=0;i<packet_cnt;i++)
+				{
+					string key_str((const char*)(traces[datafileCnt - 1][i].key), 4);
+					if(flow_ins.find(key_str)!=flow_ins.end())//如果找到
+						tag[i]=1;
+					else
+						tag[i]=0;
+				}
+
+
+
+				//测量
+				clock_gettime(CLOCK_MONOTONIC, &time1);
+				for(int t = 0; t < test_cycles; ++t)
+				{
+					//int packet_insert=0;
+					ss = new SpaceSaving<4>(TOT_MEM_IN_BYTES);
+					for(int i = 0; i < packet_cnt; ++i)
+	//					if(i%(int)floor(1.0*packet_cnt/(packet_cnt*sample/100))==0)
+						if(tag[i])
+						{
+							ss->insert(keys[i]);
+							//packet_insert++;
+						}
+					delete ss;
+					//cout<<packet_insert<<endl;
+				}
+				clock_gettime(CLOCK_MONOTONIC, &time2);
+				resns = (long long)(time2.tv_sec - time1.tv_sec) * 1000000000LL + (time2.tv_nsec - time1.tv_nsec);
+				double th = (double)1000.0 * test_cycles * packet_cnt / resns;
+				//cout<<"time:"<<resns<<endl;
+				printf("%d.dat sampling rate:%d throughput is %lf mbps\n",datafileCnt,sample,th);
+			}
+				/* free memory */
+				for(int i = 0; i < (int)traces[datafileCnt - 1].size(); ++i)
+					delete[] keys[i];
+				delete[] keys;
+
+
+
 		}
-
-		clock_gettime(CLOCK_MONOTONIC, &time1);
-		for(int t = 0; t < test_cycles; ++t)
-		{
-			ss = new SpaceSaving<4>(TOT_MEM_IN_BYTES);
-			for(int i = 0; i < packet_cnt; ++i)
-				ss->insert(keys[i]);
-			delete ss;
-		}
-		clock_gettime(CLOCK_MONOTONIC, &time2);
-		resns = (long long)(time2.tv_sec - time1.tv_sec) * 1000000000LL + (time2.tv_nsec - time1.tv_nsec);
-		double th = (double)1000.0 * test_cycles * packet_cnt / resns;
-
-		/* free memory */
-		for(int i = 0; i < (int)traces[datafileCnt - 1].size(); ++i)
-			delete[] keys[i];
-		delete[] keys;
-
-		printf("throughput is %lf mbps\n", th);
-	}
 }
 
 
